8455c7bd682cc7a463181bbd2e102ab75a98fab1
MusicDev33 <smccowan33@gmail.com>
2021-09-09 13:17:19 -0600

    More init changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from '@views/Home/Home';
+import SNavbar from '@components/SNavbar/SNavbar';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container fluid className="py-2 px-3 app-container">
+        <Row className="h-100">
+          <Col sm={3}>
+            <SNavbar />
+          </Col>
+
+          <Col sm={9}>
+            <div className="content-container px-4 py-3">
+              <Switch>
+                <Route path='/'>
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
